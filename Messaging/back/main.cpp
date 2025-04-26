da7d28f4a62cda9eb34cebc7b668e2fa9a9343b2
MRPHFitch <69053145+MRPHFitch@users.noreply.github.com>
2025-04-26 13:31:58 -0500

    Sockets established for message sending

--- Messaging/back/main.cpp
+++ Messaging/back/main.cpp
@@ -13,6 +13,9 @@
 #include <vector>
 #include <openssl/ssl.h>
 #include <openssl/err.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 #include "crypto.hpp"
 
 using namespace std;
@@ -24,74 +27,146 @@ void print_hex(const vector<unsigned char>& data) {
     printf("\n");
 }
 
+void initializeOpenssl(){
+    SSL_load_error_strings();
+    OpenSSL_add_ssl_algorithms();
+}
+
+void cleanupOpenssl(){
+    EVP_cleanup();
+}
+
+SSL_CTX* createContext(bool isServer){
+    const SSL_METHOD* method;
+    SSL_CTX* ctx;
+
+    if(isServer){
+        method=SSLv23_server_method();
+    }
+    else{
+        method=SSLv23_client_method();
+    }
+    ctx=SSL_CTX_new(method);
+    if(!ctx){
+        perror("Unable to create SSL context.");
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+    return ctx;
+}
+
+void configureContext(SSL_CTX* ctx, bool isServer){
+    if(isServer){
+        if(SSL_CTX_use_certificate_file(ctx, "server.crt", SSL_FILETYPE_PEM) <=0){
+            ERR_print_errors_fp(stderr);
+            exit(EXIT_FAILURE);
+        }
+        if(SSL_CTX_use_PrivateKey_file(ctx, "server.key", SSL_FILETYPE_PEM) <= 0){
+            ERR_print_errors_fp(stderr);
+            exit(EXIT_FAILURE);
+        }
+    }
+}
 int main() {
     // Initialize OpenSSL (for SSL/TLS, but also used by DH, AES, etc.)
-    SSL_library_init();
-    OpenSSL_add_all_algorithms();
-    OPENSSL_config(nullptr);
+    initializeOpenssl();
 
-    // Step 1: Generate Diffie-Hellman parameters (shared settings)
-    cout << "Generating Diffie-Hellman parameters...\n";
-    DH* dh = cryptography::generate_dh_params();
+    SSL_CTX* ctx=createContext(true);
+    configureContext(ctx, true);
 
-    // Step 2: Generate DH public key
-    BIGNUM* pubKeyBn = BN_new();
-    DH_get0_key(dh, &pubKeyBn, nullptr);
+    int serveSock=socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in addr;
+    addr.sin_family=AF_INET;
+    addr.sin_port=htons(49250);
+    addr.sin_addr.s_addr=htonl(INADDR_ANY);
 
-    // Step 3: Convert public key to bytes and display it
-    vector<unsigned char> pubKeyBytes(DH_size(dh));
-    int len = BN_bn2bin(pubKeyBn, pubKeyBytes.data());
-    pubKeyBytes.resize(len);
+    bind(serveSock, (struct sockaddr*)&addr, sizeof(addr));
+    listen(serveSock, 1);
 
-    cout << "My public key (hex): ";
-    print_hex(pubKeyBytes);
+    cout<<"Listening on port 49250..."<<endl;
 
-    // Step 4: Send public key to peer (simulated in this example)
-    // Normally you would send `pubKeyBytes` over a socket to the peer.
+    while(1){
+        struct sockaddr_in addr;
+        uint len=sizeof(addr);
+        SSL* ssl;
+        int cSock=accept(serveSock, (struct sockaddr*)&addr, &len);
+        ssl=SSL_new(ctx);
+        SSL_set_fd(ssl, cSock);
 
-    // Step 5: Assume we receive the peer's public key (simulated here)
-    // In a real system, this would come from the peer via network communication
-    cout << "Enter peer's public key (hex): ";
-    string peerKeyHex;
-    cin >> peerKeyHex;
+        if(SSL_accept(ssl) <= 0){
+            ERR_print_errors_fp(stderr);
+        }
+        else{
+            // Step 1: Generate Diffie-Hellman parameters (shared settings)
+            cout << "Generating Diffie-Hellman parameters...\n";
+            DH *dh = cryptography::generate_dh_params();
 
-    // Convert peer's public key from hex string to bytes
-    vector<unsigned char> peerKey = cryptography::hex_to_bytes(peerKeyHex);
+            // Step 2: Generate DH public key
+            BIGNUM *pubKeyBn = BN_new();
+            DH_get0_key(dh, &pubKeyBn, nullptr);
 
-    // Step 6: Generate shared secret using peer's public key
-    cout << "Generating shared secret...\n";
-    vector<unsigned char> sessionKey = cryptography::generate_shared_secret(dh, peerKey);
-    
-    cout << "Shared secret (hex): ";
-    print_hex(sessionKey);
+            // Step 3: Convert public key to bytes
+            vector<unsigned char> pubKeyBytes(DH_size(dh));
+            int len = BN_bn2bin(pubKeyBn, pubKeyBytes.data());
+            pubKeyBytes.resize(len);
 
-    // Step 7: Generate AES key from the shared secret (for simplicity, use part of the secret)
-    vector<unsigned char> aesKey(sessionKey.begin(), sessionKey.begin() + 32);  // AES-256 requires 32 bytes
+            //Print for debug
+            cout << "My public key (hex): ";
+            print_hex(pubKeyBytes);
 
-    cout << "AES key (hex): ";
-    print_hex(aesKey);
+            // Step 4: Send public key to peer (simulated in this example)
+            SSL_write(ssl, pubKeyBytes.data(), pubKeyBytes.size());
 
-    // Step 8: Encrypt and send a message to the peer
-    string message = "Hello, Peer! This is a secret message.";
-    vector<unsigned char> encMessage = cryptography::aes_encrypt(
-        vector<unsigned char>(message.begin(), message.end()), aesKey);
-    
-    cout << "Encrypted message (hex): ";
-    print_hex(encMessage);
+            // Step 5: Assume we receive the peer's public key (simulated here)
+            unsigned char peerKeyBytes[256];
+            int peerKeyLen=SSL_read(ssl, peerKeyBytes, sizeof(peerKeyBytes));
 
-    // Step 9: Simulate sending the encrypted message over a network
-    // Normally this would be done via a socket, but we can just "send" it here
+            vector<unsigned char> peerKey(peerKeyBytes, peerKeyBytes+peerKeyLen);
 
-    // Step 10: Decrypt the message on the receiving side (same peer)
-    vector<unsigned char> decMessage = cryptography::aes_decrypt(encMessage, aesKey);
-    
-    cout << "Decrypted message: ";
-    string decrypted_str(decMessage.begin(), decMessage.end());
-    cout << decrypted_str << endl;
+            // Step 6: Generate shared secret using peer's public key
+            cout << "Generating shared secret...\n";
+            vector<unsigned char> sharedKey = cryptography::generate_shared_secret(dh, peerKey);
 
-    // Clean up OpenSSL
-    DH_free(dh);
-    OPENSSL_cleanup();
+            //Print for debug
+            cout << "Shared secret (hex): ";
+            print_hex(sharedKey);
+
+            // Step 7: Generate AES key from the shared secret
+            vector<unsigned char> sessionKey(sharedKey.begin(), sharedKey.begin() + 32); // AES-256 requires 32 bytes
+
+            //Print for debug
+            cout << "AES key (hex): ";
+            print_hex(sessionKey);
+
+            // Step 8: Encrypt a message
+            string message = "Hello, Peer! This is a secret message.";
+            vector<unsigned char> encMessage = cryptography::aes_encrypt(
+                vector<unsigned char>(message.begin(), message.end()), sessionKey);
+
+            //Print for debug
+            cout << "Encrypted message (hex): ";
+            print_hex(encMessage);
+
+            // Step 9: Send Message
+            SSL_write(ssl, encMessage.data(), encMessage.size());
+
+            // Step 10: Decrypt the message on the receiving side (same peer)
+            vector<unsigned char> decMessage = cryptography::aes_decrypt(encMessage, sessionKey);
+
+            cout << "Decrypted message: ";
+            string decrypted_str(decMessage.begin(), decMessage.end());
+            cout << decrypted_str << endl;
+
+            // Clean up OpenSSL
+            DH_free(dh);
+        }
+        SSL_shutdown(ssl);
+        SSL_free(ssl);
+        close(cSock);
+    }
+    close(serveSock);
+    SSL_CTX_free(ctx);
+    cleanupOpenssl();
 
     return 0;
 }
