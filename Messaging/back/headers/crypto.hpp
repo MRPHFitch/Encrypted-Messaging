8d583d171fbd8a31745e8f638a78729d902c1701
quillble <nathan@ewert.com>
2025-05-04 02:25:27 -0500

    Update crypto.hpp
    Made to match new crypto.cpp. HELP NEEDED for connecting namespace to crypto.cpp functions.

--- Messaging/back/headers/crypto.hpp
+++ Messaging/back/headers/crypto.hpp
@@ -1,28 +1,42 @@
 /**
  * @file Messaging
- * @author Nathan or Mason
+ * @author Nathan
  * @version 1.0
- * @date 2025-04-16
+ * @date 2025-05-03
  * Description: Function Declarations, including necessary cryptographic functions
  * 
 */
 
 using namespace std;
 
-struct KeyPair{
-    vector<unsigned char>pubKey;
-    vector<unsigned char>priKey;
+struct RSAKeyPair{
+    X509* pubKey;
+    RSA* priKey;
 };
 
-struct EncryptedData {
+struct initationInfo {
+	vector<unsigned char> r1;
+	vector<unsigned char> signR1; 
+};
+
+struct SessionInfo {
+	vector<unsigned char> sessionKey;
+	vector<unsigned char> signR;
+};
+
+struct EncryptedMessageData {
     vector<unsigned char> ciphertext;
-    int length;
+    vector<unsigned char> hmac;
 };
 
-struct Session{
-
+// For testing
+struct Person {
+    vector<unsigned char> rootKey;
+	vector<unsigned char> messageKey;
+	vector<unsigned char> iv;
+	RSA* myPriKey;
+	X509* theirPubKey;
 };
-//Don't have to use this name, just let Matthew F. know what you do use.
 
 
 namespace cryptography{
