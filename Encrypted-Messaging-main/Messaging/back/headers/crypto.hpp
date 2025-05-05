823074a3a84408e917a9664fac799189eef16c1c
quillble <nathan@ewert.com>
2025-05-05 01:41:33 -0500

    Update crypto.hpp headers to match new functions

--- Encrypted-Messaging-main/Messaging/back/headers/crypto.hpp
+++ Encrypted-Messaging-main/Messaging/back/headers/crypto.hpp
@@ -2,7 +2,7 @@
  * @file Messaging
  * @author Nathan
  * @version 1.0
- * @date 2025-05-03
+ * @date 2025-05-04
  * Description: Function Declarations, including necessary cryptographic functions
  * 
 */
@@ -17,19 +17,19 @@
 
 using namespace std;
 
-struct RSAKeyPair {
-    RSA* priKey;
+struct RSAKeyPair{
     X509* pubKey;
+    RSA* priKey;
 };
 
-struct initiationInfo {
-    vector<unsigned char> r1;
-    vector<unsigned char> signR1;
+struct initationInfo {
+	vector<unsigned char> r1;
+	vector<unsigned char> signR1; 
 };
 
 struct SessionInfo {
-    vector<unsigned char> sessionKey;
-    vector<unsigned char> signR;
+	vector<unsigned char> sessionKey;
+	vector<unsigned char> signR;
 };
 
 struct EncryptedMessageData {
@@ -42,19 +42,29 @@ struct Person {
     vector<unsigned char> rootKey;
 	vector<unsigned char> messageKey;
 	vector<unsigned char> iv;
+	vector<unsigned char> HMACKey;
 	RSA* myPriKey;
 	X509* theirPubKey;
+	X509* serverPubKey;
+};
+struct Server {
+    RSA* serverPriKey;
+    X509* serverPubKey;
+	X509* AlicePubKey;
+	X509* BobPubKey;
 };
 
 namespace cryptography{
     // Function declarations
     RSAKeyPair generateRSAKey();
-    initiationInfo initiateSession(RSA* rsa);
+    X509* signCert(X509* signCert, RSA* key);
+    bool verifyCert(X509* cert, X509* signKey);
+    initationInfo initiateSession(X509* cert);
     SessionInfo generateSessionKey(RSA* rsa, X509* cert, vector<unsigned char> signR, vector<unsigned char> r = std::vector<unsigned char>());
-    vector<vector<unsigned char>> generateMessageKeyAndIV(vector<unsigned char> rootKey);
-    EncryptedMessageData encryptMessage(vector<unsigned char> key, vector<unsigned char> iv, string message);
-    bool verifyHMAC(vector<unsigned char> key, vector<unsigned char> data, vector<unsigned char> receivedHmac);
-    string decryptMessage(vector<unsigned char> key, vector<unsigned char> iv, vector<unsigned char> ciphertext, vector<unsigned char> hmac);
+    vector<vector<unsigned char>> generateMessageKeys(vector<unsigned char> rootKey);
+    EncryptedMessageData encryptMessage(vector<unsigned char> key, vector<unsigned char> iv, vector<unsigned char> HMAC, string message);
+    bool verifyHMAC(vector<unsigned char> HMACKey, vector<unsigned char> data, vector<unsigned char> receivedHmac);
+    string decryptMessage(vector<unsigned char> key, vector<unsigned char> iv, vector<unsigned char> HMACKey, vector<unsigned char> ciphertext, vector<unsigned char> hmac);
     void cleanup();
 }
 
@@ -64,12 +74,14 @@ private:
 
 public:
     RSAKeyPair generateRSAKey();
-    initiationInfo initiateSession(RSA* rsa);
+    X509* signCert(X509* signCert, RSA* key);
+    bool verifyCert(X509* cert, X509* signKey)
+    initationInfo initiateSession(X509* cert);
     SessionInfo generateSessionKey(RSA* rsa, X509* cert, vector<unsigned char> signR, vector<unsigned char> r = std::vector<unsigned char>());
-    vector<vector<unsigned char>> generateMessageKeyAndIV(vector<unsigned char> rootKey);
-    EncryptedMessageData encryptMessage(vector<unsigned char> key, vector<unsigned char> iv, string message);
-    bool verifyHMAC(vector<unsigned char> key, vector<unsigned char> data, vector<unsigned char> receivedHmac);
-    string decryptMessage(vector<unsigned char> key, vector<unsigned char> iv, vector<unsigned char> ciphertext, vector<unsigned char> hmac);
+    vector<vector<unsigned char>> generateMessageKeys(vector<unsigned char> rootKey);
+    EncryptedMessageData encryptMessage(vector<unsigned char> key, vector<unsigned char> iv, vector<unsigned char> HMAC, string message);
+    bool verifyHMAC(vector<unsigned char> HMACKey, vector<unsigned char> data, vector<unsigned char> receivedHmac);
+    string decryptMessage(vector<unsigned char> key, vector<unsigned char> iv, vector<unsigned char> HMACKey, vector<unsigned char> ciphertext, vector<unsigned char> hmac);
     vector<unsigned char> HKDF(vector<unsigned char> MAC);
     vector<unsigned char> chainHMAC(vector<unsigned char> rootKey, int data);
     vector<vector<unsigned char>> generateChainKeyPair(vector<unsigned char> rootKey);
